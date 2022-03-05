f99d66a4aaa6dbcc2873f7ecbe351eed6bdf81c1
Andrew <andrew_oliveira@ymail.com>
2022-03-05 14:51:24 -0300

    arquivo aula 05/03

---
+++ media5.js
@@ -0,0 +1,15 @@
+var n1 = 8;
+var n2 = 8;
+var n3 = 8;
+var n4 = 8;
+var media = 4;
+var total = (n1 + n2 + n3 + n4)/media
+if (total >= 7) {
+    console.log('Aprovado');
+}
+else if (total >= 5){
+    console.log('Recuperação');
+}
+else {
+    console.log('Reprovado');
+}
